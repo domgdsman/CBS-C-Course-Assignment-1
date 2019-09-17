73f55f77b2aaaee50d06bf218813eb13b37611ed
domgdsman <dominik.gadesmann@gmail.com>
2019-09-17 13:56:32 +0200

    first version of assignment finished

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace one
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to your Greetings App\nIf you tell me your name and time of the day. I am going to greet you smartly.\nPlease don’t forget to use 24 Hour time format (HHMM).");
+
+      System.Console.WriteLine("Please type your name: ");
+      string name = Console.ReadLine();
+
+      Console.WriteLine("Please tell me what time it is: ");
+      int time = int.Parse(Console.ReadLine());
+
+      if (time < 0 || time > 2400)
+      {
+        Console.WriteLine($"Hi {name}, don't try to fool me! Please provide a valid time in format HHMM!");
+        time = int.Parse(Console.ReadLine());
+      }
+
+      if (time >= 0 && time < 1200)
+      {
+        Console.WriteLine($"Hi {name}, Welcome and Good Morning. Have a great day!");
+      }
+
+      if (time >= 1200 && time < 1800)
+      {
+        Console.WriteLine($"Hi {name}, Welcome and Good Afternoon. Enjoy rest of the day!");
+      }
+
+      if (time >= 1800 && time <= 2400)
+      {
+        Console.WriteLine($"Hi {name}, Welcome and Have a good night!");
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# CBS Introduction to Programming and Application Design (C#)
+
+###### This is the first mandatory assignment of the course.
+
+Three reflection points about the assignment:
+
+- As I have prior coding experience I decided to stick with Visual Studio Code. Consequently I had to learn how to use .NET with VS Code, where project creation is done through console (e.g.: dotnet new console) instead of at the click of a button like in VS. Also debugging and the installation of C# plugin differs.
+- As a Javascript programmer memory allocation and type declaration of variables were new concepts to me, as there is no type declaration necessary in javascript and type coercion would be done automatically.
+- I had no problems with the understanding of the common programming concepts whatsoever.
+
+Uploaded: 17-09-2019
